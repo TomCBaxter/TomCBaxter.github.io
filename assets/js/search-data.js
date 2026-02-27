7481e9e1068bad1e9f358fb90b317b1ea8793e03
TomCBaxter <158109639+TomCBaxter@users.noreply.github.com>
2026-02-27 13:45:28 +0000

    Deploying to gh-pages from @ TomCBaxter/TomCBaxter.github.io@5bd1a38e7b4077346ee854c2027bac6c64dc894e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -465,18 +465,11 @@ ninja.data = [{
           window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/0009-0004-4738-9022", "_blank");
         },
       },{
         id: 'social-scholar',
