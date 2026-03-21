c2dcf6af0642b90c4877d57fa2437ea553937128
TomCBaxter <158109639+TomCBaxter@users.noreply.github.com>
2026-03-21 00:47:51 +0000

    Deploying to gh-pages from @ TomCBaxter/TomCBaxter.github.io@1aea5aa26a5903f1bf1c11305ac448d8f4191c4e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-notes",
           title: "Notes",
-          description: "A collection of self-contained notes, derivations, and exercises.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/notes/";
